8bb144abcc6348054bb87b3af0c0d11599354b41
Aniket Handa <atneik@gmail.com>
2013-11-17 22:32:05 -0800

    init commit

---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Aniket Handa'
+SITENAME = u'Aniket Handa'
+SITEURL = ''
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),)
+
+# Social widget
+SOCIAL = (('Facebook', 'http://fb.com/atneik'),)
+
+DEFAULT_PAGINATION = 10
+THEME = "/Users/aniket/dev/github/atneik.com---pelican/pelican-bootstrap3"
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
